3fa3ed65341cea36775e5fa6381a30258e0a5bd7
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2026-03-14 13:46:37 -0400

    feat: wrap app in language provider, and update the default path link

--- TGIF/tgif/src/App.jsx
+++ TGIF/tgif/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route,Routes } from "react-router-dom";
-import LandingPage from "./Pages/LandingPage";
+import Index from "./Pages/Index";
 import CalendarPage from "./Pages/CalenderPage";
 import EventDetailsPage from "./Pages/EventDetailsPage";
 import RegisterPage from "./Pages/RegisterPage";
@@ -9,21 +9,24 @@ import VolunteerRegistration from "./Pages/VolunteerRegistration";
 import ArtistRegistration from "./Pages/ArtistRegistration";
 import AboutPage from "./Pages/AboutPage";
 import GalleryPage from "./Pages/GalleryPage";
+import {LanguageProvider} from "./Context/LanguageContext";
 
 function App() {
     return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/events/:id" element={<EventDetailsPage />} />
-      <Route path="/register/:eventId" element={<RegisterPage />} />
-       <Route path="/register/:id" element={<RegistrationPage />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/volunteer" element={<VolunteerRegistration />} />
-      <Route path="/artist" element={<ArtistRegistration />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-    </Routes>
+        <LanguageProvider>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="/events/:id" element={<EventDetailsPage />} />
+                <Route path="/register/:eventId" element={<RegisterPage />} />
+                <Route path="/register/:id" element={<RegistrationPage />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="/volunteer" element={<VolunteerRegistration />} />
+                <Route path="/artist" element={<ArtistRegistration />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/gallery" element={<GalleryPage />} />
+            </Routes>
+        </LanguageProvider>
   ) 
 }
 
